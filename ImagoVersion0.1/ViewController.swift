0b6e247ed520b651e7d3191c22c10ce25389677f
Israel Michel Bernal Martinez <imbernal9203@gmail.com>
2016-03-12 13:27:00 -0500

    Initial Commit

---
+++ ImagoVersion0.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagoVersion0.1
+//
+//  Created by Israel Michel Bernal Martinez on 3/12/16.
+//  Copyright © 2016 Israel Michel Bernal Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
